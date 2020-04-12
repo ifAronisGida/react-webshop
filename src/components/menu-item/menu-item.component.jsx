a7e1f4d30655436aa2049d7f223849a2687c3512
ifAronisGida <arkheonos@gmail.com>
2020-04-12 19:17:56 +0200

    styled menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,14 +4,14 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
     <div 
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }} 
         className={`${size} menu-item`}>
-            <div className='content'>
-                <h1 className='title'>{title}</h1>
-                <span className='subtitle'>BUY NOW</span>
-            </div>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>BUY NOW</span>
+        </div>
     </div>
 )
 
