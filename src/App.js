7426f36542679dbfb25d9c677ad930a1584f92b4
ifAronisGida <arkheonos@gmail.com>
2020-04-12 20:15:32 +0200

    started working with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
+
+const ShopPage = () => (
+  <div>
+    <h1>SHOP PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
