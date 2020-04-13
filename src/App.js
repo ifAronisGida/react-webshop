ac4db2a5005dc494c9d2da2c97a45f0cb2b343c2
ifAronisGida <arkheonos@gmail.com>
2020-04-13 17:34:31 +0200

    added collection preview component to shop page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const ShopPage = () => (
-  <div>
-    <h1>SHOP PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
