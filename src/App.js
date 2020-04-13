169dc98fc62b25d750dcad1be8316df78447445a
ifAronisGida <arkheonos@gmail.com>
2020-04-13 19:34:44 +0200

    added custom form component w/ styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInUpPage from './pages/sign-in-up/sign-in-up.component';
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUpPage} />
       </Switch>
     </div>
   );
