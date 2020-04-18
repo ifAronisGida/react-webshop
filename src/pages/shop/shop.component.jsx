1f9ad54401cd8f6a9c88821731a09eb979ddff51
ifAronisGida <arkheonos@gmail.com>
2020-04-18 13:26:11 +0200

    updated add to cart to increase quantity

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SHOP_DATA from './shop-data';
+import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
@@ -16,13 +16,11 @@ class ShopPage extends React.Component {
 		const { collections } = this.state;
 		return (
 			<div className='shop-page'>
-				{
-					collections.map(({ id, ...otherCollectionProps }) =>
-						<CollectionPreview key={id} {...otherCollectionProps } />
-					)
-				}
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<CollectionPreview key={id} {...otherCollectionProps} />
+				))}
 			</div>
-		)
+		);
 	}
 }
 
