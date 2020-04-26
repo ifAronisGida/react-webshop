1e520f2d5b4e08eee2f7c0c9e5cb0fbdeb6d4eea
ifAronisGida <arkheonos@gmail.com>
2020-04-26 17:31:12 +0200

    added checkout page and checkout component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,10 @@ export const selectCartItemsCount = createSelector(
 			0
 		)
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce(
+		(accumulatedTotal, cartItem) => accumulatedTotal + cartItem.price * cartItem.quantity,
+		0
+	)
+);
