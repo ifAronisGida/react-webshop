a9f8a8f9550223cc0221752688889b5c1829ecbd
ifAronisGida <arkheonos@gmail.com>
2020-05-21 17:44:02 +0200

    category and shop data redux

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,17 +4,17 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreveiw = ({ title, items }) => (
+const CollectionPreview = ({ title, items }) => (
 	<div className='collection-preview'>
 		<h1 className='title'>{title.toUpperCase()}</h1>
 		<div className='preview'>
 			{items
 				.filter((item, idx) => idx < 4)
-				.map(item => (
+				.map((item) => (
 					<CollectionItem key={item.id} item={item} />
 				))}
 		</div>
 	</div>
 );
 
-export default CollectionPreveiw;
+export default CollectionPreview;
