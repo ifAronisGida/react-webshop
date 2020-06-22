a919ea79f296124db55b888c988dc95ea5f7ffcd
ifAronisGida <arkheonos@gmail.com>
2020-06-22 20:00:34 +0200

    store data in firestore and pull it into redux

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+//import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 import './App.css';
 
